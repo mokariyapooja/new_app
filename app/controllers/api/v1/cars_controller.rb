b24d445f5cadc12e2bb805e28cf50de0085bd8ff
pooja <pooja.apsolute@gmail.com>
2014-10-14 16:22:14 +0530

    minor changes

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -4,9 +4,7 @@ class Api::V1::CarsController < Api::V1::BaseController
   def create_car
     @car = Car.create(car_params)
     if @car.save
-      Rails.logger.debug "===========#{@car.inspect}============"
       name = @car.name
-      Rails.logger.debug "===========#{name.inspect}============"
       render :file => 'api/v1/cars/show'            
     else
       render_json({:errors => @car.display_errors, :status => 404}.to_json)   
@@ -17,7 +15,6 @@ class Api::V1::CarsController < Api::V1::BaseController
   def car_list
     @cars = Car.all
     if @cars.present?
-      Rails.logger.debug "===========#{@cars.inspect}============"
       render :file => 'api/v1/cars/index'
     else
       render_json({:errors => "No cars yet.!", :status => 404}.to_json)
